a849834f080e36b8e1767b6ac93b1787dada989e
Rushikesh chavan <rushikeshchavan4566@gmail.com>
2020-11-06 10:52:23 +0530

    example is commited

---
+++ example.java
@@ -0,0 +1,7 @@
+class example{
+    public static void printEvenNumbers(int no) {
+        system.out.println("Even numbers upto" + no);
+        for(int x=0; x < no; x = x+2)
+        system.out.println(x);
+    }
+}
